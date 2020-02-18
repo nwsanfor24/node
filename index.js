e7bbfdcf94bbbdf27e72eff12d98a2de4b195e2d
Nathan Sanford <nwsanfor24@gmail.com>
2020-02-17 19:28:51 -0500

    add message.txt

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-fs.appendFile('message.txt', 'data to append', function (err) {
+fs.appendFile('message.txt', ' data to append', function (err) {
   if (err) throw err;
   console.log('Saved!');
 });
