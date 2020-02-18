320eedef42e558d7a94c85f45038a038456c2cbd
Nathan Sanford <nwsanfor24@gmail.com>
2020-02-17 19:31:58 -0500

    more changes

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ fs.appendFile('message.txt', ' data to append', function (err) {
   if (err) throw err;
   console.log('Saved!');
 });
+
+fs.write('message.txt', ' new line added' + "\n", 'utf8', function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+});
